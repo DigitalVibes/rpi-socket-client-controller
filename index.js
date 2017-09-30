c46680338249191d1ed6f6f934c968ebedd7bd2c
pavan-08 <pavanchhatpar@gmail.com>
2017-09-30 12:38:29 +0530

    command line arguments for host, port and protocol

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-var socket = require('socket.io-client')('http://192.168.1.111:4200');
+function findIndex(args, arg) {
+
+    var len = args.length,
+        i = 0;
+
+    for (; i < len; i++) {
+        if (args[i].match(arg)) {
+            return i;
+        }
+    }
+
+    return -1;
+
+};
+var hi = findIndex(process.argv, '--host');
+var httpsi = findIndex(process.argv, '--https');
+var pi = findIndex(process.argv, '--port');
+
+var host = (hi === -1 ? '--host=localhost' : process.argv[hi]).split('=')[1];
+var port = (pi === -1 ? '--port=4200' : process.argv[pi]).split('=')[1];
+var protocol = httpsi === -1 ? 'http': 'https';
+
+var socket = require('socket.io-client')(protocol + '://' + host + ':' + port);
 const five = require('johnny-five');
 const raspi = require('raspi-io');
 const board = new five.Board({
